66247b2ebf5fd69dbc70adbfb7afdf2d7021a257
Johannes Losch <hi@jlosch.de>
2021-12-20 22:54:07 +0100

    added footer

--- src/App.vue
+++ src/App.vue
@@ -166,6 +166,49 @@
       </div>
     </div>
   </div>
+  <div class="background footer">
+    <div class="content">
+      <div class="left">
+        <a>Impress</a>
+        <a>Terms of Service</a>
+        <a>Data protection</a>
+        <a class="help">Help</a>
+        <a>Copyright 2021 Johannes Losch</a>
+      </div>
+      <div class="middle">
+        <a href="https://linkedin.com/in/johannes-losch" target="_blank">
+          <div class="link">
+            <img src="https://s.jlosch.de/links/linkedin.svg" loading="lazy" />
+            LinkedIn
+          </div>
+        </a>
+        <a href="https://johanneslosch.medium.com/" target="_blank">
+          <div class="link">
+            <img src="https://s.jlosch.de/links/medium.svg" loading="lazy" />
+            Medium
+          </div>
+        </a>
+        <a href="https://github.com/johanneslosch" target="_blank">
+          <div class="link">
+            <img src="https://s.jlosch.de/links/github.svg" loading="lazy" />
+            Github
+          </div>
+        </a>
+        <a href="https://twitter.com/johanneslosch" target="_blank">
+          <div class="link">
+            <img src="https://s.jlosch.de/links/twitter.svg" loading="lazy" />
+            Twitter
+          </div>
+        </a>
+      </div>
+      <div class="right">
+        <img
+          src="https://s.jlosch.de/logo/logo_johanneslosch_untereinander.svg"
+          loading="lazy"
+        />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -213,7 +256,7 @@ export default {
 
 .landing {
   background: $bg_blue;
-  margin-top: $nav_height;
+  margin-top: -1%;
   z-index: 1;
 
   .content {
@@ -381,6 +424,77 @@ export default {
           }
         }
       }
+    }
+  }
+}
+
+.footer {
+  height: 300px;
+  background: $bg_blue;
+  margin-bottom: -5%;
+
+  .content {
+    padding-top: 2rem;
+    display: flex;
+    flex-direction: row;
+    text-decoration: none;
+
+    a {
+      font-size: 16pt;
+      color: $text-color;
+    }
+
+    .left {
+      flex-grow: 1;
+      gap: 1rem;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      width: 33.333%;
+
+      .help {
+        cursor: help;
+      }
+    }
+
+    .middle {
+      a {
+        text-decoration: none;
+
+        &:hover {
+        }
+      }
+
+      .link {
+        img {
+          width: 6%;
+          padding-right: 1rem;
+
+          &:hover {
+            transform: scale(1);
+          }
+        }
+      }
+
+      flex-grow: 1;
+      gap: 1rem;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      width: 33.333%;
+    }
+
+    .right {
+      img {
+        width: 40%;
+      }
+
+      flex-grow: 1;
+      gap: 1rem;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      width: 33.333%;
     }
   }
 }
